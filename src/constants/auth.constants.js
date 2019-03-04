d5343725cd9051380a0dc07e2b6e60463575202f
German Hernandez <muygrafico@gmail.com>
2019-03-04 16:13:20 -0500

    basic redux setup

---
+++ src/constants/auth.constants.js
@@ -0,0 +1,5 @@
+export const authConstants = {
+    START: 'ALERT_START',
+    SUCCESS: 'ALERT_SUCCESS',
+    ERROR: 'ALERT_ERROR'
+};
---
+++ src/reducers/auth.js
@@ -0,0 +1,39 @@
+import { authConstants } from '../constants';
+
+const initialAuthState = {
+  error: {
+    message: ''
+  },
+  isLoggedIn: false
+};
+
+
+const auth = (state = initialAuthState, action) => {
+  switch (action.type) {
+    case authConstants.START:
+      return [
+        ...state,
+        {
+          authenticaded: action.id,
+        }
+      ]
+    case authConstants.SUCCESS:
+      return [
+        ...state,
+        {
+          authenticaded: action.id,
+        }
+      ]
+    case authConstants.ERROR:
+      return [
+        ...state,
+        {
+          authenticaded: action.id,
+        }
+      ]
+    default:
+      return state
+  }
+}
+
+export default auth
