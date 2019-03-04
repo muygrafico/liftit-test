63eb4140ea22b9a0affb3cb323f65f573a006dc3
German Hernandez <muygrafico@gmail.com>
2019-03-04 15:35:06 -0500

    basic setup done

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,29 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
 
 module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
       }
     ]
-  }
+  },
+  plugins: [htmlPlugin]
 };
