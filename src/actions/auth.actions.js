819035bd78a2515b3dec10b944ac53df0e0a04b5
German Hernandez <muygrafico@gmail.com>
2019-03-04 16:12:19 -0500

    basic redux setup

---
+++ src/actions/auth.actions.js
@@ -0,0 +1,19 @@
+import { authConstants } from '../constants';
+
+export const alertActions = {
+    success,
+    error,
+    start
+};
+
+function success(message) {
+    return { type: authConstants.SUCCESS, message };
+}
+
+function error(message) {
+    return { type: authConstants.ERROR, message };
+}
+
+function start(message) {
+    return { type: authConstants.START, message };
+}
