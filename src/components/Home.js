79c8eb6a72f6d233280a837787fd44f9c4428f36
German Hernandez <muygrafico@gmail.com>
2019-03-06 10:32:41 -0500

    update styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,35 @@
+import React, { Component } from 'react'
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react'
 
-import React from 'react'
+// const LoggedIn = () => (
+//   <div>
+//     <h1>Home...YEY! </h1>
+//   </div>
+// )
 
-const LoggedIn = () => (
-  <div>
-    <h1>Home...YEY! </h1>
-  </div>
-)
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+}
 
-export default LoggedIn
+export class MapContainer extends Component {
+  render () {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={{
+          lat: -1.2884,
+          lng: 36.8233
+        }}
+      />
+    )
+  }
+}
+
+// export default LoggedIn
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyCqlLhE4Fs9dLAiuS65NTTTe_udBr5VfE8'
+})(MapContainer)
